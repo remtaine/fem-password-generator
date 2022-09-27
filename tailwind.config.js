a0ae83ab11abd00c97130a9bc20bc2b4c857510e
Niel Benjamin Kho <benjokho@gmail.com>
2022-09-28 00:36:55 +0800

    Add default links

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
@@ -28,14 +29,14 @@ module.exports = {
     extend: {
       colors: {
         fem: {
-          100: "",
-          200: "",
-          300: "",
-          400: "",
-          500: "",
-          600: "",
-          700: "",
-          800: "",
+          100: "#24232C", //bg
+          200: "#817D92", //title and STRENGTH
+          300: "#E6E5EA", //normal text
+          400: "#18171F", //STRENGTH bg
+          500: "#A4FFAF", //accent and 4 bars
+          600: "#F64A4A", //1 bar
+          700: "#FB7C58", //2 bars
+          800: "#F8CD65", //3 bars
           900: "",
         },
       },
