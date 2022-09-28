de03e5a3fe7ce828ab1ddb5f41fec587038d8a9f
Niel Benjamin Kho <benjokho@gmail.com>
2022-09-28 09:35:58 +0800

    Change to input tag with accent

--- src/components/ui/Checkbox.js
+++ src/components/ui/Checkbox.js
@@ -4,36 +4,38 @@ function Checkbox() {
   const [checked, setChecked] = useState(false);
 
   return (
-    // <input
-    //   type="checkbox"
-    //   onChange={() => {
-    //     setChecked(!checked);
-    //   }}
-    //   className={
-    //     "appearance-none h-2 w-2 bg-fem-300 text-fem-100 border border-solid border-2 border-fem-500" +
-    //     " " +
-    //     (checked ? "" : "")
-    //   }
-    // />
-    // TODO change border to outline
-    <button
-      onClick={() => {
+    <input
+      type="checkbox"
+      onChange={() => {
         setChecked(!checked);
       }}
       className={
-        "flex items-center justify-center w-[10px] h-[10px] outline outline-[1.5px]" +
+        "accent-fem-500 scale-125 bg-fem-300 text-fem-100 border border-solid border-2 border-fem-500" +
         " " +
-        (checked ? "bg-fem-500 outline-fem-500" : "outline-fem-300")
+        (checked ? "" : "")
       }
-    >
-      <i
-        className={
-          "fa-solid fa-check text-[8px] text-fem-400" +
-          " " +
-          (checked ? "block" : "hidden")
-        }
-      ></i>
-    </button>
+    />
+
+    // TODO change border to outline
+
+    // <button
+    //   onClick={() => {
+    //     setChecked(!checked);
+    //   }}
+    //   className={
+    //     "flex items-center justify-center w-[10px] h-[10px] outline outline-[1.5px]" +
+    //     " " +
+    //     (checked ? "bg-fem-500 outline-fem-500" : "outline-fem-300")
+    //   }
+    // >
+    //   <i
+    //     className={
+    //       "fa-solid fa-check text-[8px] text-fem-400" +
+    //       " " +
+    //       (checked ? "block" : "hidden")
+    //     }
+    //   ></i>
+    // </button>
   );
 }
 
