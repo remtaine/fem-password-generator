ac299042c1b7594f12a04a198c59156598fda55c
Niel Benjamin Kho <benjokho@gmail.com>
2022-09-28 14:25:27 +0800

    Fix hook reactions on generate button click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Bars from "./components/Bars";
 import BarScore from "./components/BarScore";
@@ -17,17 +17,11 @@ function App() {
 
   const [copyActive, setCopyActive] = useState(false);
   const [password, setPassword] = useState("P4$5W0rD!1");
-  // TODO add password length from slider
-  const [passwordLength, setPasswordLength] = useState(10);
-  // TODO add hasCharacterPool from checkboxes
-  const [hasCharacterPool, setHasCharacterPool] = useState([
-    false,
-    false,
-    false,
-    false,
-  ]);
+  const [passwordScore, setPasswordScore] = useState(3);
 
-  // TODO setup context
+  useEffect(() => {
+    setPasswordScore(getPasswordStrength());
+  }, [password]);
 
   const flashCopy = function () {
     navigator.clipboard.writeText(password);
@@ -41,22 +35,26 @@ function App() {
 
   // TODO setup password generator based on values
   const generatePassword = function () {
-    if (passwordLength === 0 || characterPoolTotal.length === 0) return "";
+    // TODO if (NO CHECKBOXES ARE CHECKED) return "";
 
-    return "LEZGO";
+    const answer = "ban";
+    console.log(answer);
+    return answer;
   };
 
-  const getPasswordStrength = function (passwordLength, hasCharacterPool) {
-    // passwordLength is slider value
-    // hasCharacterPool is based on checkboxes
-    // characterPoolTotalLength is sum of characterPoolLength[i] * hasCharacterPool[i]
+  const getPasswordStrength = function () {
+    console.log("checking password: " + password);
+    // characterPoolTotalLength is sum of each characterPoolLength[i] if hasCharacterPool[i]
 
     // formula is:
     // entropy = passwordLength * log(characterPoolTotalLength) / log(2)
+    // TODO change characterPool total based on hasCharacterPool based on MANUAL SEARCH NOT CHECKBOXES
+    let hasCharacterPool = [false, false, false, false];
+
     let entropy =
-      passwordLength === 0 || characterPoolTotal.length === 0
+      characterPoolTotal.length === 0
         ? 0
-        : passwordLength * (Math.log(characterPoolTotal.length) / Math.log(2));
+        : password.length * (Math.log(characterPoolTotal.length) / Math.log(2));
 
     // 25 identifies a poor password SCORE = 1
     // between 25 and 50 is a weak password SCORE = 2
@@ -71,13 +69,20 @@ function App() {
 
   return (
     <div className="App bg-fem-400 min-h-screen flex items-center justify-center selection:bg-fem-500 selection:text-fem-400">
-      <main className="mx-1 flex flex-col gap-2 max-w-[360px] w-full text-fem-300 px-2">
+      <main className="font-JetBrains mx-1 flex flex-col gap-2 max-w-[360px] w-full text-fem-300 px-2">
         <h1 className="mx-auto text-fem-200 text-base">Password Generator</h1>
-        <div className="bg-fem-100 flex justify-between pl-4 pr-2 items-center shadow ">
-          <span className="">{password}</span>
+        <div className="bg-fem-100 flex pl-4 pr-2 items-center shadow ">
+          <input
+            type="text"
+            className="bg-transparent accent-fem-300 shrink min-w-0"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
           <button
             onClick={flashCopy}
-            className="px-2 py-3 flex gap-2 items-center text-fem-500"
+            className="px-2 py-3 flex gap-2 items-center text-fem-500 flex-none"
           >
             <span
               className={
@@ -102,15 +107,16 @@ function App() {
           <div className="bg-fem-400 px-2 py-3 flex items-center justify-between">
             <span className="uppercase text-fem-200 text-xs">Strength</span>
             <div className="flex gap-1 items-center">
-              <BarScore
-                score={getPasswordStrength(passwordLength, hasCharacterPool)}
-              />
-              <Bars
-                score={getPasswordStrength(passwordLength, hasCharacterPool)}
-              />
+              <BarScore score={passwordScore} />
+              <Bars score={passwordScore} />
             </div>
           </div>
-          <button className="bg-fem-500 text-fem-400 hover:bg-fem-100 outline-fem-500 outline outline-1 duration-300 hover:text-fem-500 uppercase flex items-center justify-center w-full gap-2 py-1 px-2">
+          <button
+            onClick={() => {
+              setPassword(generatePassword());
+            }}
+            className="bg-fem-500 text-fem-400 hover:bg-fem-100 outline-fem-500 outline outline-1 duration-300 hover:text-fem-500 uppercase flex items-center justify-center w-full gap-2 py-1 px-2"
+          >
             <span className="text-xs py-2">Generate</span>
             <i className="fa-solid fa-arrow-right text-xs"></i>
           </button>
